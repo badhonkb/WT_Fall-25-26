018bde6b6761b5328104ebf543fe1009e90073c7
Badhon_Kumar_Biswas <bbadhon733@gmail.com>
2025-11-02 12:30:38 +0600

    Lab Task 1

--- Mid/Lab_Task_1/lab_task_1.php
+++ Mid/Lab_Task_1/lab_task_1.php
@@ -1,1 +1,59 @@
+<!DOCTYPE html>
+<head>
+    <title>Registration Form</title>
+</head>
 
+
+    <center>
+
+<h2 style = "color : Blue">Clinic Patient Registration</h2>
+
+<body>
+</center>
+Full Name:    <br></br>
+<input type ="text"><br></br>
+Age : <br> </br>
+<input type ="text"><br></br>
+
+Phone Number : <br> </br>
+<input type ="text"><br></br>
+
+Email Address : <br> </br>
+<input type ="text"><br></br>
+
+Insurance provider : <br> </br>
+<select>
+    <option> Select Provider </option>
+    <option> Mutual Trust Bank</option>
+    <option> United Commercial Bank</option>
+</select>
+<br> </br>
+Insurance Policy Number : <br> </br>
+<input type ="text"><br></br>
+
+<center>
+   <h2 style = "color : Blue">Additional Information</h2>
+
+</center>
+
+Username: <br> </br>
+<input type ="text"><br></br>
+
+Password: <br> </br>
+<input type ="text"><br></br>
+
+
+Confrim Password: <br> </br>
+<input type ="text"><br></br>
+
+
+
+
+<center>
+    <button type="submit" style="background-color: blue;">Register</button>
+</center>
+
+
+
+
+</body>
